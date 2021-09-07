3d58cd962c1943f03089b8a957f883639f5dcd2a
bills-sys <85712144+bills-sys@users.noreply.github.com>
2021-09-06 22:46:04 -0300

    Up

--- src/main/java/com/github/billssys/waffle/application/App.java
+++ src/main/java/com/github/billssys/waffle/application/App.java
@@ -5,7 +5,17 @@ package com.github.billssys.waffle.application;
  *
  */
 public class App {
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static int foo() {
+        int result = 0;
+        for (int i=1; i<21; i++) {
+            result += Math.pow(2, i);
+        }
+        return result;
+    }
+
 }
